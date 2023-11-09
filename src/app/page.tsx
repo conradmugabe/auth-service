036b23b2c68b740a524408b56157c74993aac1bb
conrad mugabe <conradmugabe@gmail.com>
2023-11-09 23:30:20 +0300

    fix: prettier fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { LoginProviders } from "./_components/login-providers";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen flex-col items-center justify-center gap-10 bg-white text-dark">
+    <main className="text-dark flex h-screen w-screen flex-col items-center justify-center gap-10 bg-white">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Welcome to an Auth Service
       </h1>
