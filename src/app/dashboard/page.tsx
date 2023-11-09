c0e0d7dbf18bc2e8b145ddb14f0b44ca5e940448
conrad mugabe <conradmugabe@gmail.com>
2023-11-09 23:18:38 +0300

    feat: added xata db

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,26 @@
 import { redirect } from "next/navigation";
 
 import { UserButton, auth } from "@clerk/nextjs";
+import { getXataClient } from "@/db";
 
-export default function Page() {
+export default async function Page() {
+  const client = getXataClient();
   const { userId } = auth();
   if (!userId) redirect("/");
+
+  const posts = await client.db.Posts.getAll();
 
   return (
     <main>
       <nav>
         <UserButton />
       </nav>
-      <section className="flex min-h-screen items-center justify-center">
+      <section className="flex flex-col gap-10 min-h-screen items-center justify-center">
         <p className="text-2xl">
           userId <span className="font-bold">{userId}</span>
+        </p>
+        <p className="text-2xl">
+          posts count <span className="font-bold">{posts.length}</span>
         </p>
       </section>
     </main>
