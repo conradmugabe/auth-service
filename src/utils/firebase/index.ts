fdfc6d0f36e0208edd64e02829edd6ff70975b57
conrad mugabe <conradmugabe@gmail.com>
2025-03-07 22:36:04 +0300

    breaking: start project from scratch to build from the backend only

--- src/utils/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-import { initializeApp } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
-
-import { firebaseConfig } from "./config";
-
-const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-
-export { firestore };
