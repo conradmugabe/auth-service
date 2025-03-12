ba833b39df0c83a6a52c6bbb926f9a645f9c7c1f
conrad mugabe <conradmugabe@gmail.com>
2025-03-12 15:50:49 +0300

    feat: implement AuthRouter with login, register, logout, and getMe endpoints; add corresponding tests

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 
+import { combineRouters } from './server/http/utils';
+import { AuthRouter } from './server/http/auth/index';
+
 const PORT = 3000;
 
 export function app(): express.Application {
   const _app = express();
+
+  combineRouters([new AuthRouter()]);
 
   return _app;
 }
